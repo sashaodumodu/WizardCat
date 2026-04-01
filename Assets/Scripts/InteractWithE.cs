170281cac3d7ab1f121e057faffc60a60003cddf
sashaodumodu <189272304+sashaodumodu@users.noreply.github.com>
2026-04-01 02:17:06 -0400

    Dialogue System
    Created a simple dialogue system. Integrated into the E to interact script, may remove later or edit to create a seperate script.

--- Assets/Scripts/InteractWithE.cs
+++ Assets/Scripts/InteractWithE.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class InteractWithE : MonoBehaviour
 {
+    [Header("Interaction")] //Dialogue system
+    [SerializeField] public GameObject targetPanel; //Dialogue system
     private bool playerInRange = false;
 
     void Update()
@@ -9,6 +12,7 @@ public class InteractWithE : MonoBehaviour
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("E pressed - interaction works!");
+            TogglePanel();
         }
     }
 
@@ -25,6 +29,16 @@ public class InteractWithE : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
+        }
+    }
+    
+    //Dialogue system
+    public void TogglePanel()
+    {
+        if (targetPanel != null)
+        {
+            // Toggles the active state of the panel (true/false)
+            targetPanel.SetActive(!targetPanel.activeSelf);
         }
     }
 }
