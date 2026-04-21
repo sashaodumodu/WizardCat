e70549d1ffc229d6e35b85e3ad07f20faf8fa00e
Khadijah B <232601918+kbashir2-ux@users.noreply.github.com>
2026-04-21 12:51:41 -0400

    Created Settings UI!!
    Toggle between Credits and Controls

--- Assets/Scripts/UIs/MenuManage.cs
+++ Assets/Scripts/UIs/MenuManage.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuManage : MonoBehaviour
 {
+    [Header("Settings UI")]
+    public GameObject mainMenuUI;
+    public GameObject settingsPopup;
+    public GameObject creditsPanel;
+    public GameObject controlsPanel;
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene("TestScene");
@@ -14,6 +20,28 @@ public class MenuManage : MonoBehaviour
 
     public void Settings()
     {
+        mainMenuUI.SetActive(false);
 
+        settingsPopup.SetActive(true);
+        creditsPanel.SetActive(true);
+        controlsPanel.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(true);
+        controlsPanel.SetActive(false);
+    }
+
+    public void ShowControls()
+    {
+        creditsPanel.SetActive(false);
+        controlsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPopup.SetActive(false);
+        mainMenuUI.SetActive(true);
     }
 }
