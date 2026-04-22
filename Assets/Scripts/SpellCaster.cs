34a32d2b58432b8cfc7e3871e38d45b27f42d3d3
Olaoluwa221 <oifeiji@emich.edu>
2026-04-22 03:29:00 -0400

    Changed cast button to right-click

--- Assets/Scripts/SpellCaster.cs
+++ Assets/Scripts/SpellCaster.cs
@@ -12,7 +12,7 @@ public class SpellCaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(1))
         {
             Instantiate(spell);
         }
